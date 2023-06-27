ea8469ef265fe218a0dcbf8b414d480a58a62a52
sugarbeet <katsuranaoyuki@gmail.com>
2023-06-27 17:16:02 +0900

    server.tsの追加

---
+++ backend/server.ts
@@ -0,0 +1,39 @@
+import path from "node:path";
+
+import cors from "cors";
+import express from "express";
+
+import type { Express, Request, Response } from "express";
+
+// import { Todo } from "../types"
+
+const app: Express = express();
+const port = process.env.PORT ?? 3001
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../frontend')))
+
+interface Todo { id: number; title: string; completed: boolean };
+
+const sampleData: Todo[] = [
+  { id: 1, title: "1つめ", completed: false },
+  { id: 2, title: "2つめ", completed: true },
+  { id: 3, title: "3つめ", completed: false },
+  { id: 4, title: "4つめ", completed: false },
+]
+
+const todoList: Todo[] = sampleData
+
+// localhost:3001/todo-listにアクセスするとToDoリストを返す
+// ポート番号も自分で変更可能
+app.get("/todo-list", (req: Request, res: Response) => {
+  res.json(todoList)
+})
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello World!");
+// });
+
+app.listen(port, () => {
+  console.log(`${port}でサーバー起動中`);
+});
