a925e9bcc89d47d427be5838b752401d4f4a08d9
sugarbeet <katsuranaoyuki@gmail.com>
2023-06-28 10:37:40 +0900

    表示内容の変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
-import Home from "@components/home";
 import { getElementById } from "@utils/dom/get-element-by-id";
 
+import App from "./app/index"
 import { store } from "./store";
 
 getElementById("app").match(
   (element) =>
     createRoot(element).render(
       <Provider store={store}>
-        {/* <App /> */}
-        <Home />
+        <App />
+        {/* <Home /> */}
       </Provider>
     ),
   (err) => alert(err)
